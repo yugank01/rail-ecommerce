2ae9cc8df0e63d1538142249dd11d792f5bc38c3
Yugank Bansal <99734769+yugank01@users.noreply.github.com>
2024-07-18 12:05:07 +0530

    Add files via upload

---
+++ app/api/add_product/route.ts
@@ -0,0 +1,30 @@
+import Product from "@/libs/models/Product";
+import { connectMongoDB } from "@/libs/MongoConnect";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(request: NextRequest) {
+  try {
+    const body = await request.json();
+    const { imgSrc, fileKey, name, category, price } = body;
+
+    await connectMongoDB();
+
+    const data = await Product.create({
+      imgSrc,
+      fileKey,
+      name,
+      category,
+      price,
+    });
+
+    return NextResponse.json({ msg: "Product added Successfully!", data });
+  } catch (error) {
+    return NextResponse.json(
+      {
+        error,
+        msg: "Something Went Wrong",
+      },
+      { status: 400 }
+    );
+  }
+}
---
+++ app/api/delete_product/[id]/route.ts
@@ -0,0 +1,23 @@
+import Product from "@/libs/models/Product";
+import { connectMongoDB } from "@/libs/MongoConnect";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function DELETE(request: NextRequest, URLParams: any) {
+  try {
+    const id = URLParams.params.id;
+
+    await connectMongoDB();
+
+    await Product.findByIdAndDelete(id);
+
+    return NextResponse.json({ msg: "Product Deleted Successfully!" });
+  } catch (error) {
+    return NextResponse.json(
+      {
+        error,
+        msg: "Something Went Wrong",
+      },
+      { status: 400 }
+    );
+  }
+}
